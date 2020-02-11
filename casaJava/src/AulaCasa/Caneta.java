558e15c219abe0424bfec6a709bb2ae76290b130
4ZUK <60348334+4ZUK@users.noreply.github.com>
2020-02-11 20:56:04 -0300

    casao

---
+++ casaJava/src/AulaCasa/Caneta.java
@@ -0,0 +1,23 @@
+package AulaCasa;
+
+public class Caneta{
+    public class String cor;
+    public class String modelo;
+    public class int carga;
+    public class boolean tampada;
+    public class float ponta;
+
+    void tampar(){
+
+    }
+
+
+    void rabiscar() {
+
+    }
+
+    void destampar() {
+
+    }
+
+}
